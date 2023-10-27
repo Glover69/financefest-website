4f5b9d55bb1207e4ec475bf691ec4449401d145c
Daniel Glover <iamglover02@gmail.com>
2023-10-27 01:57:32 +0000

    updates to website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,27 @@ import { gsap } from 'gsap';
 import TextPlugin from 'gsap/TextPlugin'; // Import the TextPlugin
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
-
 gsap.registerPlugin(TextPlugin); // Register the TextPlugin
 gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
   title = 'FinanceFast-Website';
 
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {}
 
   ngAfterViewInit(): void {
     this.fadeInFromLeft();
     this.fadeInFromCenter();
-    // this.typeText();
     this.blinkCursor();
     this.mockups();
     this.partners();
     this.carousel();
+    this.featureContainer();
   }
 
   // Fade in animation for the main hero text
@@ -49,9 +48,8 @@ export class AppComponent implements AfterViewInit {
     gsap.to(blueTextelement, {
       delay: 6,
       text: this.textTwo,
-      duration: this.text.length * 0.05,
+      duration: this.textTwo.length * 0.1,
       ease: 'power1.in',
-
     });
 
     gsap.to(cursor, {
@@ -79,7 +77,7 @@ export class AppComponent implements AfterViewInit {
       y: -20,
       duration: 0.75,
       delay: 10, // Animation duration in seconds
-      ease: 'power1.inOut' // Easing function
+      ease: 'power1.inOut', // Easing function
     });
 
     gsap.to(element, {
@@ -87,7 +85,7 @@ export class AppComponent implements AfterViewInit {
       y: 0,
       duration: 0.75,
       delay: 10, // Animation duration in seconds
-      ease: 'power1.inOut' // Easing function
+      ease: 'power1.inOut', // Easing function
     });
   }
 
@@ -101,7 +99,7 @@ export class AppComponent implements AfterViewInit {
       x: 0,
       duration: 1,
       delay: 10, // Animation duration in seconds
-      ease: 'power1.inOut' // Easing function
+      ease: 'power1.inOut', // Easing function
     });
   }
 
@@ -150,67 +148,16 @@ export class AppComponent implements AfterViewInit {
       x: 0,
       duration: 0.75,
       delay: 11, // Animation duration in seconds
-      ease: 'power1.inOut' // Easing function
+      ease: 'power1.inOut', // Easing function
     });
   }
-
-  // carousel() {
-  //   const carousel = this.el.nativeElement.querySelector('.carousel');
-  //   const images = Array.from(carousel.querySelectorAll('.logo'));
-
-  //   // Calculate the total width of the carousel
-  //   const totalWidth : any = Array.from(images).reduce((acc, img: any) => acc + img.offsetWidth + 12, 0); // 12 is the gap between images
-
-  //   // Set the container width to the total width of all images
-  //   carousel.style.width = totalWidth + '%';
-
-  //   // Create a GSAP timeline for the animation
-  //   const timeline = gsap.timeline({
-  //     repeat: 1, // Repeat infinitely
-  //     defaults: { ease: 'linear' }
-  //   });
-
-  //   // Animation to move the carousel to the left
-  //   timeline.to(carousel, {
-  //     x: -totalWidth, // Move the container to the left by the total width of all images
-  //     duration: 50, // Adjust the duration for the speed of the carousel (50 pixels per second in this case)
-  //     scrollTrigger: {
-  //       trigger: carousel,
-  //       start: 'start end', // Start the animation when the container starts and end when it ends
-  //       scrub: true, // Smoothly scrub through the animation as the user scrolls
-  //     }
-  //   });
-
-  //   // Animation to reset the carousel position to the start
-  //   timeline.set(carousel, { x: 0 });
-  // }
-
-  // carousel() {
-  //   const element = this.el.nativeElement.querySelector('.carousel');
-
-  //   gsap.fromTo(element, {
-  //     xPercent: 100,
-  //   }, {
-  //     xPercent: -100,
-  //     duration: 10,
-  //     ease: 'linear',
-  //     repeat: -1,
-  //     // scrollTrigger: {
-  //     //   trigger: '.carousel',
-  //     //   start: 'start end',
-  //     //   scrub: true,
-  //     //   pin: true,
-  //     //   // anticipatePin: 1,
-  //     // },
-  //   });
-  // }
 
   private carousel(): void {
     const carousel = this.el.nativeElement.querySelector('.carousel');
     const items = this.el.nativeElement.querySelectorAll('.carousel-item');
 
     // Calculate total width of marquee
-    const totalWidth : any = Array.from(items).reduce((acc, item:any) => {
+    const totalWidth: any = Array.from(items).reduce((acc, item: any) => {
       acc += item.offsetWidth;
       return acc;
     }, 0);
@@ -228,18 +175,87 @@ export class AppComponent implements AfterViewInit {
     // Create a GSAP timeline
     const timeline = gsap.timeline({
       repeat: -1, // Repeat infinitely
-      defaults: { ease: 'linear' }
+      defaults: { ease: 'linear' },
     });
 
     // Animation to move the marquee to the left
     timeline.to(carousel, {
+      yoyo: true,
       x: -carousel.style.width, // Move the marquee to the left by the total width of items
       duration: carousel.style.width / 100, // Adjust the duration for the speed of the marquee (100 pixels per second in this case)
       scrollTrigger: {
         trigger: carousel,
         start: 'start end', // Start the animation when the marquee starts and end when it ends
         scrub: true, // Smoothly scrub through the animation as the user scrolls
-      }
+      },
+    });
+  }
+
+  featureContainer() {
+    const mainHeaderText = 'Say Goodbye to International Payment Hassles';
+    const featureSection = document.querySelector('.feature-section');
+    const mainHeader = document.querySelector('.feature-section-mainHeader');
+    const subHeader = document.querySelector('.feature-section-subheader');
+
+    gsap.from(featureSection, {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      delay: 10,
+      scrollTrigger: {
+        trigger: featureSection,
+        start: 'top bottom-=200',
+        end: 'bottom center',
+        scrub: true,
+      },
+    });
+
+    gsap.to(featureSection, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      delay: 10,
+      scrollTrigger: {
+        trigger: featureSection,
+        start: 'top bottom-=200',
+        end: 'bottom center',
+        scrub: true,
+      },
+    });
+
+    gsap.from(subHeader, {
+      opacity: 0,
+      y: -100,
+      duration: 1,
+      scrollTrigger: {
+        trigger: subHeader,
+        start: 'top bottom-=200',
+        end: 'bottom center',
+        scrub: true,
+      },
+    });
+
+    gsap.to(subHeader, {
+      opacity: 1,
+      y: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: subHeader,
+        start: 'top bottom-=200',
+        end: 'bottom center',
+        scrub: true,
+      },
+    });
+
+    gsap.to(mainHeader, {
+      text: mainHeaderText,
+      duration: mainHeaderText.length * 0.1,
+      scrollTrigger: {
+        trigger: mainHeader,
+        start: 'top bottom-=200',
+        end: 'bottom center',
+        // scrub: true,
+      },
     });
   }
 }
