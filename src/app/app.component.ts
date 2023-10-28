0f752a13713ddaaf158a01b407bbd14672fde509
Daniel Glover <iamglover02@gmail.com>
2023-10-28 19:01:53 +0000

    UI + Animation Updates #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { gsap, random } from 'gsap';
 import TextPlugin from 'gsap/TextPlugin';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import CSSRulePlugin from 'gsap/CSSRulePlugin';
+import SplitType from 'split-type'
+
 // import CustomBounce from 'gsap-trial/CustomBounce';
 // import CustomEase from 'gsap-trial/CustomEase';
 
@@ -69,28 +71,46 @@ export class AppComponent implements AfterViewInit {
 
   // Fade in animation for the main hero text
 
-  private text = 'Simplify Your International Transactions with'; // Text for the type animation
-  private textTwo = 'One Platform'; // ext for the type animation (Blue text)
+  private text = 'Simplify Your International Transactions with One Platform'; // Text for the type animation
+  private textTwo = ''; // ext for the type animation (Blue text)
   private speed = 0.1; // Typing speed (lower value for faster typing)
   private cursorBlinkSpeed = 0.5; // Cursor blinking speed
+  // private splitText = require('split-text');
+  
+  
+ 
 
   private blinkCursor(): void {
-    const cursor = CSSRulePlugin.getRule('.hero-text-wrapper::after');
+    const cursor = this.el.nativeElement.querySelector('.cursor');
     const element = this.el.nativeElement.querySelector('.mainHeroText');
     const blueTextelement = this.el.nativeElement.querySelector('.blue-text');
+    // const textThree = SplitType.create('.partners-txt', {types: 'chars'});
 
     gsap.to(element, {
       delay: 2,
       text: this.text,
       duration: this.text.length * this.speed,
       ease: 'power1.in',
-    });
 
-    gsap.to(blueTextelement, {
-      delay: 6,
-      text: this.textTwo,
-      duration: this.textTwo.length * 0.1,
-      ease: 'power1.in',
+      onComplete: () => {
+        // Animation is complete
+        // Change the color of 'One Platform'
+        const worldIndex = this.text.indexOf('One Platform');
+        const worldSpan = document.createElement('span');
+        worldSpan.textContent = 'One Platform';        // worldSpan.style.color = '#563ACC'; // Set the desired color
+
+        gsap.to(worldSpan, {
+          color: '#563ACC',
+          delay: 1,
+          duration: 0.75,
+
+          onComplete: () => {
+            element.innerHTML = this.text.substring(0, worldIndex) + worldSpan.outerHTML;
+          }   
+        
+        });
+      }
+      
     });
 
     gsap.to(cursor, {
@@ -111,7 +131,7 @@ export class AppComponent implements AfterViewInit {
       opacity: 1,
       y: 0,
       duration: 1,
-      delay: 8, // Animation duration in seconds
+      delay: 9, // Animation duration in seconds
       ease: 'power1.inOut', // Easing function
     });
   }
@@ -125,8 +145,8 @@ export class AppComponent implements AfterViewInit {
       'clip-path': 'polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)',
       opacity: 1,
       y: 0,
-      duration: 1.5,
-      delay: 8, // Animation duration in seconds
+      duration: 1.25,
+      delay: 9, // Animation duration in seconds
       ease: 'power3.inOut', // Easing function
     });
   }
@@ -137,8 +157,8 @@ export class AppComponent implements AfterViewInit {
     gsap.to(element, {
       opacity: 1,
       y: 0,
-      duration: 1.5,
-      delay: 8, // Animation duration in seconds
+      duration: 1.25,
+      delay: 9, // Animation duration in seconds
       ease: 'power4.inOut',
     });
   }
@@ -149,14 +169,13 @@ export class AppComponent implements AfterViewInit {
     gsap.to(element, {
       width: '100%',
       opacity: 1,
-      x: 0,
       duration: 1,
       delay: 8, // Animation duration in seconds
       ease: 'power1.inOut', // Easing function
       onComplete: () => {
         // Animation complete callback
         // Change the page's overflow property to 'scroll'
-        document.body.style.overflow = 'scroll';
+        document.body.style.overflowY = 'scroll';
         this.el.nativeElement.querySelector('.feature-section').style.display =
           'flex';
       },
@@ -164,14 +183,41 @@ export class AppComponent implements AfterViewInit {
   }
 
   bounce() {
-    // CustomBounce.create('myBounce', {
-      // strength: 0.5,
-       // squash: 0,
-      // squashID: 'myBounce-squash',
-    // }); 
+    CustomBounce.create('myBounce', {
+      strength: 0.5,
+      squash: 0,
+      squashID: 'myBounce-squash',
+    });
 
-    // gsap.from('.white-container',{
-    //   y: -150
-    // })
+    gsap.to('.white-container', {
+      delay: 0.5,
+      duration: 5,
+      y: '0',
+      // rotate: ,
+      transformOrigin: 'top center',
+      ease: 'myBounce',
+      repeat: 0,
+      repeatDelay: 1.5,
+      stagger: 0.25,
+      scrollTrigger: {
+        trigger: '.white-container',
+        // pin the trigger element while active
+        start: 'top 300px',
+        end: 'top 100px', // end after scrolling 500px beyond the start
+        markers: true,
+      },
+    });
+
+    //the squish which affects scaleX and scaleY. To make the effect stronger/weaker, just change the scaleX/scaleY values:
+    gsap.to('.white-container', {
+      delay: 0.2,
+      duration: 6,
+      scaleX: 1,
+      scaleY: 1,
+      ease: 'myBounce-squash',
+      transformOrigin: 'bottom',
+      repeat: 0,
+      repeatDelay: 1.5,
+    });
   }
 }
